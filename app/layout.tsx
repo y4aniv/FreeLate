f574ad26f669b2c109e25fd06d293e54f8dc5b86
Yaniv <120222659+y4aniv@users.noreply.github.com>
2025-09-15 11:28:14 +0000

    chore: initialize project with Tailwind CSS, TypeScript, and global styles
    - Added PostCSS configuration for Tailwind CSS.
    - Included favicon.ico for branding.
    - Created global CSS file to import Tailwind CSS styles.
    - Set up TypeScript configuration with strict settings and module resolution.

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "@/styles/globals.css"
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dm-sans"
+})
+
+const metadata: Metadata = {
+  title: "FreeLate – La galère est réelle. L’attestation aussi."
+}
+
+const RootLayout = ({children}: {children: React.ReactNode}) => {
+  return <html lang={"fr"}>
+    <body className={`${dmSans.variable} antialiased`}>
+      {children}
+    </body>
+  </html>
+}
+
+export {metadata}
+export default RootLayout;
