68536266b221ccab398c55c5c5c9def60e7de955
Yaniv <120222659+y4aniv@users.noreply.github.com>
2025-09-15 14:54:07 +0000

    feat(ui): enhance form with incident types and duration selection

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,32 @@
 import { Button, Center, Select, Stack, TextInput } from "@mantine/core";
 import { DateTimePicker } from "@mantine/dates";
 import Logo from "@/components/logo";
-import { LINES } from "@/lib/constants";
+import { DURATION, INCIDENTS, LINES } from "@/lib/constants";
 import { randomAgentId } from "@/lib/utils";
 
 const Root = () => {
 	return (
-		<Center className="h-dvh">
+		<Center className="min-h-dvh">
 			<Stack className="w-full max-w-lg p-4">
-				<Logo />
+				<Center className="mb-4">
+					<Logo />
+				</Center>
 				<Select
 					label={"Ligne de transport"}
 					placeholder={"3bis, 9, RER A, ..."}
 					searchable
-					data={LINES.map((line) => ({ label: line.name, value: line.value }))}
+					data={LINES.map((line) => ({ label: line.name, value: line.id }))}
 				/>
 				<Select
 					label={"Nature de l'incident"}
 					placeholder={"Mouvement sociale, panne de signalisation, ..."}
 					searchable
+					data={INCIDENTS.sort((a, b) => a.name.localeCompare(b.name)).map(
+						(incident) => ({
+							label: incident.name,
+							value: incident.id,
+						}),
+					)}
 				/>
 				<TextInput
 					label={"Identité du bénéficiaire"}
@@ -28,7 +36,14 @@ const Root = () => {
 					label={"Date & heure de délivrance de l'attestation"}
 					placeholder={"21/03/1948 00:00"}
 				/>
-				<Select label={"Durée de l'incident"} placeholder={"30 minutes"} />
+				<Select
+					label={"Durée de l'incident"}
+					placeholder={"Supérieure à 15 minutes"}
+					data={DURATION.map((duration) => ({
+						label: duration.name,
+						value: duration.id,
+					}))}
+				/>
 				<TextInput
 					label={"Matricule de l’agent émetteur"}
 					defaultValue={randomAgentId()}
@@ -37,6 +52,13 @@ const Root = () => {
 				<Button fullWidth type="submit">
 					{"Générer mon attestation"}
 				</Button>
+				<p className="text-xs text-gray-500 text-center">
+					En cliquant sur "Générer mon attestation", l’utilisateur reconnaît
+					être seul responsable des informations fournies et de l’utilisation
+					qui en est faite, et accepte que l’attestation générée n’a aucune
+					valeur légale ou officielle et ne peut en aucun cas être utilisée à
+					des fins administratives, juridiques ou réglementaires.
+				</p>
 			</Stack>
 		</Center>
 	);
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,120 +1,234 @@
 const LINES = [
 	{
 		name: "1",
-		value: "1",
+		id: "1",
 	},
 	{
 		name: "2",
-		value: "2",
+		id: "2",
 	},
 	{
 		name: "3",
-		value: "3",
+		id: "3",
 	},
 	{
 		name: "3bis",
-		value: "3bis",
+		id: "3bis",
 	},
 	{
 		name: "4",
-		value: "4",
+		id: "4",
 	},
 	{
 		name: "5",
-		value: "5",
+		id: "5",
 	},
 	{
 		name: "6",
-		value: "6",
+		id: "6",
 	},
 	{
 		name: "7",
-		value: "7",
+		id: "7",
 	},
 	{
 		name: "7bis",
-		value: "7bis",
+		id: "7bis",
 	},
 	{
 		name: "8",
-		value: "8",
+		id: "8",
 	},
 	{
 		name: "9",
-		value: "9",
+		id: "9",
 	},
 	{
 		name: "10",
-		value: "10",
+		id: "10",
 	},
 	{
 		name: "11",
-		value: "11",
+		id: "11",
 	},
 	{
 		name: "12",
-		value: "12",
+		id: "12",
 	},
 	{
 		name: "13",
-		value: "13",
+		id: "13",
 	},
 	{
 		name: "14",
-		value: "14",
+		id: "14",
 	},
 	{
 		name: "RER A",
-		value: "rer_a",
+		id: "rer_a",
 	},
 	{
 		name: "RER B",
-		value: "rer_b",
+		id: "rer_b",
 	},
 	{
 		name: "RER C",
-		value: "rer_c",
+		id: "rer_c",
 	},
 	{
 		name: "T1",
-		value: "t1",
+		id: "t1",
 	},
 	{
 		name: "T2",
-		value: "t2",
+		id: "t2",
 	},
 	{
 		name: "T3a",
-		value: "t3a",
+		id: "t3a",
 	},
 	{
 		name: "T3b",
-		value: "t3b",
+		id: "t3b",
 	},
 	{
 		name: "T4",
-		value: "t4",
+		id: "t4",
 	},
 	{
 		name: "T5",
-		value: "t5",
+		id: "t5",
 	},
 	{
 		name: "T6",
-		value: "t6",
+		id: "t6",
 	},
 	{
 		name: "T7",
-		value: "t7",
+		id: "t7",
 	},
 	{
 		name: "T8",
-		value: "t8",
+		id: "t8",
 	},
 	{
 		name: "T9",
-		value: "t9",
+		id: "t9",
 	},
 ];
 
-export { LINES };
+const INCIDENTS = [
+	{
+		name: "Malaise voyageur",
+		id: "passenger_illness",
+	},
+	{
+		name: "Panne de signalisation",
+		id: "signal_failure",
+	},
+	{
+		name: "Panne électrique",
+		id: "power_failure",
+	},
+	{
+		name: "Incident technique",
+		id: "technical_issue",
+	},
+	{
+		name: "Train en panne",
+		id: "train_breakdown",
+	},
+	{
+		name: "Colis suspect",
+		id: "suspicious_package",
+	},
+	{
+		name: "Incident d'exploitation",
+		id: "operational_issue",
+	},
+	{
+		name: "Voyageur sur la voie",
+		id: "person_on_tracks",
+	},
+	{
+		name: "Mesure de sécurité",
+		id: "safety_measure",
+	},
+	{
+		name: "Divers incidents",
+		id: "various_incidents",
+	},
+	{
+		name: "Incident voyageur",
+		id: "passenger_incident",
+	},
+	{
+		name: "Accident grave de voyageur",
+		id: "serious_passenger_accident",
+	},
+	{
+		name: "Panne de matériel",
+		id: "equipment_failure",
+	},
+	{
+		name: "Mouvement social",
+		id: "strike",
+	},
+	{
+		name: "Obstacle sur la voie",
+		id: "obstacle_on_tracks",
+	},
+	{
+		name: "Acte de malveillance",
+		id: "malicious_act",
+	},
+	{
+		name: "Dégagement de fumée",
+		id: "smoke_release",
+	},
+	{
+		name: "Intervention des équipes techniques",
+		id: "technical_team_intervention",
+	},
+	{
+		name: "Panne de caténaire",
+		id: "catenary_failure",
+	},
+	{
+		name: "Déclenchement d'un signal d'alarme",
+		id: "alarm_triggered",
+	},
+	{
+		name: "Bagage oublié",
+		id: "forgotten_luggage",
+	},
+	{
+		name: "Manifestation",
+		id: "protest",
+	},
+	{
+		name: "Animal sur la voie",
+		id: "animal_on_tracks",
+	},
+	{
+		name: "Accident grave de personne",
+		id: "serious_personal_accident",
+	},
+	{
+		name: "Personnes sur les voies",
+		id: "people_on_tracks",
+	},
+];
+
+const DURATION = [
+	{
+		name: "Supérieure à 15 minutes",
+		id: "over_15_minutes",
+	},
+	{
+		name: "Supérieure à 30 minutes",
+		id: "over_30_minutes",
+	},
+];
+
+export { LINES, INCIDENTS, DURATION };
