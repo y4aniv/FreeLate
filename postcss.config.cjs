d86146c8a5b739cd3c669e01eaeff36b379e2d3b
Yaniv <120222659+y4aniv@users.noreply.github.com>
2025-09-15 12:43:47 +0000

    chore(biome): update schema version to 2.2.0

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-  plugins: {
-	'@tailwindcss/postcss': {},
-    'postcss-preset-mantine': {},
-    'postcss-simple-vars': {
-      variables: {
-        'mantine-breakpoint-sm': '40rem',
-        'mantine-breakpoint-md': '48rem',
-        'mantine-breakpoint-lg': '64rem',
-        'mantine-breakpoint-xl': '80rem',
-      },
-    },
-  },
+	plugins: {
+		"@tailwindcss/postcss": {},
+		"postcss-preset-mantine": {},
+		"postcss-simple-vars": {
+			variables: {
+				"mantine-breakpoint-sm": "40rem",
+				"mantine-breakpoint-md": "48rem",
+				"mantine-breakpoint-lg": "64rem",
+				"mantine-breakpoint-xl": "80rem",
+			},
+		},
+	},
 };
